36ddde03f1f9cb9247a61aa6e919ac6cb8f627f4
Varun Kumar <varuntheace@gmail.com>
2024-06-29 11:35:50 +0530

    Final!!

--- basic_functions.py
+++ basic_functions.py
@@ -1,103 +1,60 @@
-from lastfmapi import GetGenre
 import random
 import pygame
-
-lastfm = GetGenre()
-artist, song = lastfm.last_fm_playing()
-get_genre = lastfm.track_get_info(artist, song)
-print(get_genre)
-
+from lastfmapi import GetGenre
 
 class SlimeSelect:
     def __init__(self, genre):
         self.path = 'Assets/'
         self.genre = genre
-        self.genre = self.genre
-        self.green_tags = ('folk', 'ambient', 'experimental', 'singer-songwriter', 'acoustic', 'chillout')
-        self.blue_tags = ('electronic', 'indie', 'pop', 'ambient', 'electronica', 'house', 'techno')
-        self.red_tags = ('rock', 'metal', 'punk', 'hardcore', 'thrash metal', 'progressive rock', 'heavy metal')
-        self.all_tags = self.red_tags + self.green_tags + self.blue_tags
-        self.current_slime = ''
-        self.slimes = ['Blue_Slime', 'Green_Slime', 'Red_Slime']
-        self.count = 0
+        self.green_tags = {'folk', 'ambient', 'experimental', 'singer-songwriter', 'acoustic', 'chillout'}
+        self.blue_tags = {'electronic', 'indie', 'pop', 'electronica', 'house', 'techno'}
+        self.red_tags = {'rock', 'metal', 'punk', 'hardcore', 'thrash metal', 'progressive rock', 'heavy metal'}
+        self.all_tags = self.red_tags | self.green_tags | self.blue_tags
+        self.slimes = {
+            'Blue_Slime': (self.blue_tags, '/Run', 7),
+            'Green_Slime': (self.green_tags, '/Walk', 8),
+            'Red_Slime': (self.red_tags, '/Jump', 13)
+        }
+        self.current_slime = 'Blue_Slime'
+        self.current_action = '/Run'
+        self.frame_count = 7
+
+        self.select_slime()
 
     def select_slime(self):
         if self.genre not in self.all_tags:
-            self.current_slime_index = random.randint(0, len(self.slimes) - 1)
-            self.current_slime = self.slimes[self.current_slime_index]
-            self.action = self.random_todo()
-            self.count = self.frame_count()
-            self.current_slime_dir = self.path + self.current_slime + self.action
-            self.current_slime_images = []
-            while len(self.current_slime_images) != self.count:
-                for i in range(1, self.count + 1):
-                    self.current_slime_image_temp = pygame.image.load(
-                        self.current_slime_dir + '/' + f"{self.action}{i}.png").convert_alpha()
-                    self.current_slime_image_temp = pygame.transform.scale(self.current_slime_image_temp, (150, 150))
-                    self.current_slime_images.append(self.current_slime_image_temp)
-            return self.current_slime_images
+            self.current_slime = random.choice(list(self.slimes.keys()))
+            self.current_action, self.frame_count = self.random_todo()
         else:
-            if self.genre in self.red_tags:
-                self.current_slime = self.slimes[2]
-                self.action = '/Jump'
-                self.count = 13
-                self.current_slime_dir = self.path + self.current_slime + self.action
-                self.current_slime_images = []
-                while len(self.current_slime_images) != self.count:
-                    for i in range(1, self.count + 1):
-                        self.current_slime_image_temp = pygame.image.load(
-                            self.current_slime_dir + '/' + f"{self.action}{i}.png").convert_alpha()
-                        self.current_slime_image_temp = pygame.transform.scale(self.current_slime_image_temp,
-                                                                               (150, 150))
-                        self.current_slime_images.append(self.current_slime_image_temp)
-                return self.current_slime_images
-            elif self.genre in self.green_tags:
-                self.current_slime = self.slimes[1]
-                self.action = '/Walk'
-                self.count = 8
-                self.current_slime_dir = self.path + self.current_slime + self.action
-                self.current_slime_images = []
-                while len(self.current_slime_images) != self.count:
-                    for i in range(1, self.count + 1):
-                        self.current_slime_image_temp = pygame.image.load(
-                            self.current_slime_dir + '/' + f"{self.action}{i}.png").convert_alpha()
-                        self.current_slime_image_temp = pygame.transform.scale(self.current_slime_image_temp,
-                                                                               (150, 150))
-                        self.current_slime_images.append(self.current_slime_image_temp)
-                return self.current_slime_images
-            elif self.genre in self.blue_tags:
-                self.current_slime = self.slimes[0]
-                self.action = '/Run'
-                self.count = 7
-                self.current_slime_dir = self.path + self.current_slime + self.action
-                self.current_slime_images = []
-                while len(self.current_slime_images) != self.count:
-                    for i in range(1, self.count + 1):
-                        self.current_slime_image_temp = pygame.image.load(
-                            self.current_slime_dir + '/' + f"{self.action}{i}.png").convert_alpha()
-                        self.current_slime_image_temp = pygame.transform.scale(self.current_slime_image_temp,
-                                                                               (150, 150))
-                        self.current_slime_images.append(self.current_slime_image_temp)
-                return self.current_slime_images
+            for slime, (tags, action, count) in self.slimes.items():
+                if self.genre in tags:
+                    self.current_slime = slime
+                    self.current_action = action
+                    self.frame_count = count
+                    break
+
+        self.current_slime_images = self.load_images()
+
+    def load_images(self):
+        images = []
+        slime_dir = f"{self.path}{self.current_slime}{self.current_action}"
+        for i in range(1, self.frame_count + 1):
+            image_path = f"{slime_dir}/{self.current_action}{i}.png"
+            image = pygame.image.load(image_path).convert_alpha()
+            image = pygame.transform.scale(image, (150, 150))
+            images.append(image)
+        return images
 
     def random_todo(self):
-        self.to_do_list = ['/Run', '/Jump', '/Walk']
-        self.to_do = random.choice(self.to_do_list)
-        if self.to_do == '/Jump':
-            self.count = 13
-        elif self.to_do == '/Walk':
-            self.count = 8
-        else:
-            self.count = 7
-        return self.to_do
+        to_do_list = [
+            ('/Run', 7),
+            ('/Jump', 13),
+            ('/Walk', 8)
+        ]
+        return random.choice(to_do_list)
 
-    def frame_count(self):
-        if self.genre in self.red_tags:
-            self.count = 13
-            return self.count
-        elif self.genre in self.green_tags:
-            self.count = 8
-            return self.count
-        else:
-            self.count = 7
-            return self.count
+    def get_images(self):
+        return self.current_slime_images
+
+    def get_frame_count(self):
+        return self.frame_count
--- game.py
+++ game.py
@@ -1,11 +1,11 @@
 import pygame
 import random
+import time
 from lastfmapi import GetGenre
 from basic_functions import SlimeSelect
 import win32gui
 import win32api
 import win32con
-
 
 pygame.init()
 
@@ -16,53 +16,73 @@ info = pygame.display.Info()
 screen = pygame.display.set_mode((WIDTH, HEIGHT), pygame.NOFRAME)
 screen.fill(BG_COLOR)
 
-
-
 x_pos = random.randint(0, WIDTH - 150)
 y_pos = random.randint(0, HEIGHT - 150)
 x_speed = random.choice([-1, 1]) * random.uniform(1, 3)
 y_speed = random.choice([-1, 1]) * random.uniform(1, 3)
 
 
-def green(x_pos, y_pos, x_speed, y_speed):
-    x_pos = x_pos + x_speed
-    y_pos = y_pos + y_speed
+def update_position(x_pos, y_pos, x_speed, y_speed, slime_type):
+    if slime_type == 'Blue_Slime':
+        speed_multiplier = 1
+    elif slime_type == 'Green_Slime':
+        speed_multiplier = 1
+    elif slime_type == 'Red_Slime':
+        speed_multiplier = 1.5
+    else:
+        speed_multiplier = 1
+
+    x_pos += x_speed * speed_multiplier
+    y_pos += y_speed * speed_multiplier
     if x_pos <= 0 or x_pos >= WIDTH - 150:
         x_speed = -x_speed
     if y_pos <= 0 or y_pos >= HEIGHT - 150:
         y_speed = -y_speed
     return x_pos, y_pos, x_speed, y_speed
 
+
+song_info = GetGenre()
+artist, song = song_info.last_fm_playing()
+genre = song_info.track_get_info(artist, song)
+slime_selector = SlimeSelect(genre)
+slime_images = slime_selector.get_images()
+frame_index = 0
+frame_count = slime_selector.get_frame_count()
+
+clock = pygame.time.Clock()
+animation_speed = 12
+
+hwnd = pygame.display.get_wm_info()["window"]
+win32gui.SetWindowLong(hwnd, win32con.GWL_EXSTYLE,
+                       win32gui.GetWindowLong(hwnd, win32con.GWL_EXSTYLE) | win32con.WS_EX_LAYERED)
+win32gui.SetLayeredWindowAttributes(hwnd, win32api.RGB(255, 255, 255), 0, win32con.LWA_COLORKEY)
+win32gui.SetWindowPos(hwnd, win32con.HWND_TOPMOST, 0, 0, 0, 0, win32con.SWP_NOMOVE | win32con.SWP_NOSIZE)
+
+last_song_check = time.time()
+check_interval = 5  # Check for song change every 5 seconds
+
 run = True
 while run:
-    song_info = GetGenre()
-    artist, song = song_info.last_fm_playing()
-    genre = song_info.track_get_info(artist, song)
-    slime = SlimeSelect(genre).select_slime()
-
-    frame_index = 0
-    frame_count = SlimeSelect(genre).frame_count()
-    clock = pygame.time.Clock()
-    animation_speed = 12
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
 
-    if SlimeSelect(genre).current_slime == 'Blue_Slime':
-        x_pos, y_pos, x_speed, y_speed = green(x_pos, y_pos, x_speed, y_speed)
+    if time.time() - last_song_check >= check_interval:
+        artist, song = song_info.last_fm_playing()
+        new_genre = song_info.track_get_info(artist, song)
+        if new_genre != genre:
+            genre = new_genre
+            slime_selector = SlimeSelect(genre)
+            slime_images = slime_selector.get_images()
+            frame_count = slime_selector.get_frame_count()
+        last_song_check = time.time()
 
-    screen.fill(BG_COLOR)
-
-    hwnd = pygame.display.get_wm_info()["window"]
-    win32gui.SetWindowLong(hwnd, win32con.GWL_EXSTYLE,
-                           win32gui.GetWindowLong(hwnd, win32con.GWL_EXSTYLE) | win32con.WS_EX_LAYERED)
-    win32gui.SetLayeredWindowAttributes(hwnd, win32api.RGB(255, 255, 255), 0, win32con.LWA_COLORKEY)
-    win32gui.SetWindowPos(hwnd, win32con.HWND_TOPMOST, 0, 0, 0, 0, win32con.SWP_NOMOVE | win32con.SWP_NOSIZE)
+    x_pos, y_pos, x_speed, y_speed = update_position(x_pos, y_pos, x_speed, y_speed, slime_selector.current_slime)
 
     frame_index = (frame_index + 1) % frame_count
 
-    screen.blit(slime[frame_index], (x_pos, y_pos))
+    screen.fill(BG_COLOR)
+    screen.blit(slime_images[frame_index], (x_pos, y_pos))
     pygame.display.flip()
 
     clock.tick(animation_speed)
